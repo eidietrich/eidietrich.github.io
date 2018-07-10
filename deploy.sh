cec5a0db6b914f3fc1a1e7bf637cc3c219823f45
Eric Dietrich <eric.dietrich@gmail.com>
2018-07-09 18:32:53 -0600

    Refresh site

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,23 @@
 
 # bash deploy.sh
 
-# BUILDDIR=./_site/.
-# DESTDIR=./../eidietrich.github.io
-# RETURNPATH=./../portfolio-site-jekyll
-
-# jekyll build
-
-# cp -R $BUILDDIR $DESTDIR
-# cd $DESTDIR
-# git add *
-# git commit -m "Scripted update"
-# git push
-# cd $RETURNPATH
-# echo "deploy complete"
 
 # New deployment process
 # Ref https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
 
+# Update _site folder and hold
+bundle exec jekyll build
 echo "ericidietrich.com" >> ./_site/CNAME
+git add _site
+git commit -m "Refresh site"
 
+# Push to "prod" remote
 git subtree split --prefix _site -b gh-pages
-git push -f prod gh-pages:master
+git push prod gh-pages:master
 git branch -D gh-pages
+
+# To do force overwrite of
+# git push -f prod gh-pages:master
+
+# To set up "prod" remote
+git remote add prod https://github.com/eidietrich/eidietrich.github.io.git
